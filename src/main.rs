c72d848cd519129edb56dbdb123032c2e346a43a
mambo-dev <mambo.michael.22@gmail.com>
2023-08-13 21:52:08 +0300

    feat: read file contents

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use std::env;
+use std::fs;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
     let query = &args[1];
     let file_path = &args[2];
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+
+    let contents = fs::read_to_string(file_path).expect("should have read the file");
+
+    println!("with text\n:{contents}");
 }
